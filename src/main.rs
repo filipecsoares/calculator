a65980d8dc06d53950a4db312a119fab71fc53df
filipecsoares <filipecarlossoares@gmail.com>
2023-09-10 14:29:32 -0300

    feat: get expression from input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Welcome to Rust calculator!");
+    println!("Welcome to Rust calculator!\n");
+    println!("Enter the mathematical expression:");
+    let mut expression = String::new();
+    io::stdin().read_line(&mut expression).expect("Failed to read line");
+    println!("Result: {}", expression);
 }
